df7cc475668c04a973893a020ddec36d85ec6107
Noemí Vargas <mimi2002@estudiantec.cr>
2023-11-11 22:24:40 -0600

    API for SQL

---
+++ StraviaTecAPI/StraviaTecAPI/Controllers/TodoApp.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StraviaTecAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoApp : ControllerBase
+    {
+    }
+}
