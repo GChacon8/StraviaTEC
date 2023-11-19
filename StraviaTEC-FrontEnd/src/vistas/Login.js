5b638683f06256930561b4f3fe731f5fcaa281fc
jpesquetini <85376273+jpesquetini@users.noreply.github.com>
2023-11-19 17:24:38 -0600

    loginXbase

--- StraviaTEC-FrontEnd/src/vistas/Login.js
+++ StraviaTEC-FrontEnd/src/vistas/Login.js
@@ -1,14 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link, useNavigate } from 'react-router-dom';
 import '../Css/Styles.css'; 
 import Cookies from 'js-cookie';
+import axios from 'axios';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
+
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:7170/api/Deportista')
+      .then(response => {
+         setResult(response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+
+   var UserInfo = [];
+
+  for (let i = 0; i < result.length; i++) {
+     const datos = {
+       User: result[i].usuario,
+       Password: result[i].contrasena,
+     }
+     UserInfo.push(datos);
+   }
+    
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -21,16 +45,21 @@ function Login() {
 
   //Verificación del usuario y contraseña del usuario
   const handleSubmit = () => {
-    if (password === 'admin') {
-   
-      Cookies.set('userInfo', username, { expires: 1 });
-      console.log('Cookie creada');
-      navigate('/StraviaTec');
-    } else {
-      console.log('Credenciales incorrectas');
-      setErrorMessage('Usuario o contraseña incorrectos');
-      //navigate('/ClientView');
+    for (let i = 0; i < UserInfo.length; i++) {
+      if (UserInfo[i].User === username){
+        if (UserInfo[i].Password === password) {
+          Cookies.set('userInfo', username, { expires: 1 });
+          console.log('Cookie creada');
+          navigate('/StraviaTec');
+        } else {
+          console.log('Credenciales incorrectas');
+          setErrorMessage('Usuario o contraseña incorrectos');
+        }
+      }
     }
+    setErrorMessage('Usuario o contraseña incorrectos');
+
+    
   };
 
   const handleCreateUser =()=>{
